a880c4a72e4c86ba8a43479ff8b5865ec44658b3
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-17 14:47:59 -0600

    the tenth commitment

--- src/App.js
+++ src/App.js
@@ -53,17 +53,6 @@ class App extends Component {
         <Header></Header>
         <Dashboard></Dashboard>
         <Form></Form>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
