e27aec1e1dbbd675e49f6ddfb6419c8c2081d008
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-17 14:40:41 -0600

    ninth life of a CAT

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env
 // const user_ctrl = require('./user.controllers')
 
 app.use(express.json())
+
+app.post('/api/dashboard', choose_ctrl.postDashboard)
+app.get('/api/form', choose_ctrl.getForm)
+app.post('api/header', choose_ctrl.postHeader)
+app.get('/api/product', choose_ctrl.getProduct)
 // database is a reference to the database opn Heroku
 massive(CONNECTION_STRING).then((database) => {
     app.set('db', database)
@@ -13,5 +18,3 @@ massive(CONNECTION_STRING).then((database) => {
     app.listen(process.env.SERVER_PORT, () => console.log(`IN LOVE WITH CHELLIE PETE _${SERVER_PORT}`))
 })
 
-// const PORT = 3456
-// app.listen(PORT, () => console.log(`Caleb is a frickin boss ${PORT}`))
--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Components/Header/Header'
 import Dashboard from './Components/Dashboard/Dashboard'
 import Form from './Components/Form/Form'
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+    
+    }
+  }
+
+  handlePostDashboard = () => {
+    axios.get('/api/dashboard').then((res) => {
+      this.setState({
+        dashboard: res.data
+      })
+    })
+  }
+
+  handleGetForm = () => {
+    axios.get('/api/form').then((res) => {
+      this.setState({
+        form: res.data
+      })
+    })
+  }
+
+  handlePostHeader = () => {
+    axios.get('/api/header').then((res) => {
+      this.setState({
+        header: res.data
+      })
+    })
+  }
+
+  handleGetProduct= () => {
+    axios.get('/api/product').then((res) => {
+      this.setState({
+        product: res.data
+      })
+    })
+  }
+
+  render() { 
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -24,6 +67,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
